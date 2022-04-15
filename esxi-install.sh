49a0b63e1bc7f3f199568f5ca04a8f3c7062140b
fengzhao <fengzhao1124@hotmail.com>
2022-04-15 15:54:54 +0800

    Update esxi-install.sh

--- esxi-install.sh
+++ esxi-install.sh
@@ -11,5 +11,8 @@ echo Powered by sysin.org
 export PATH=/bin:/sbin:/usr/bin:/usr/sbin
 
 echo Installing unlocker.tgz
+
+
+# 调用ESXI中的/BootModuleConfig.sh来加载unlocker.tgz文件
 BootModuleConfig.sh --verbose --add=unlocker.tgz
 echo Success - please now restart the server!
